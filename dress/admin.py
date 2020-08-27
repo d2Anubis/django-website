c3b726bddfbacb095e5cb3c963c8be608282c0aa
Niharika <59616618+d2Anubis@users.noreply.github.com>
2020-08-27 23:12:37 +0530

    first commit
    first commit

---
+++ dress/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Category,Dress,Fashion
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Dress)
+admin.site.register(Fashion)
